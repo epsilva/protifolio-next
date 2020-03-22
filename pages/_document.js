6f3b97cbb2365f3af4431e16c155c5b75599f3f2
Esdras Pinheiro <eps.94@hotmail.com>
2020-03-22 10:14:11 -0300

    Test head

--- pages/_document.js
+++ pages/_document.js
@@ -15,22 +15,24 @@ export default class MyDocument extends Document {
     }
 
     render() {
-        const retorno = {
-            about: 'Esdras',
-            apresentation: 'Sou Eu',
-            img: 'https://firebasestorage.googleapis.com/v0/b/portfolio-esdras.appspot.com/o/esdras-pinheiro%2FimgHome.jpeg?alt=media&token=66d76474-c2bb-48d6-a20e-8b4a5a5a9bbd'
-        }
         return (
             <html>
                 <Head>
-                    {this.props.styleTags}
-                    <meta charSet="utf-8" />
-                    <title>Esdras Pinheiro</title>
-                    <link rel="canonical" href="https://portifolio-esdras.herokuapp.com/profile/esdras-pinheiro" />
-                    <meta property="og:description" content={`${retorno.about?.replace(/<[^>]*>?/gm, '')}`} />
-                    <meta property="og:site_name" content={`${retorno.apresentation} - Veja Nossas Vagas!`} />
-                    <meta property="og:title" content={`${retorno.apresentation}`} />
-                    <meta property="og:image" content={`${retorno.img}`} />
+                    <link rel="shortcut icon" href="https://cdn.sstatic.net/Sites/br/img/favicon.ico?v=c6678b633455" />
+                    <link rel="apple-touch-icon" href="https://cdn.sstatic.net/Sites/br/img/apple-touch-icon.png?v=e57f45c9cfaf" />
+                    <link rel="image_src" href="https://cdn.sstatic.net/Sites/br/img/apple-touch-icon.png?v=e57f45c9cfaf" />
+                    <link rel="search" type="application/opensearchdescription+xml" title="Stack Overflow em Portugues" href="/opensearch.xml" />
+                    <link rel="canonical" href="https://pt.stackoverflow.com/questions/59135/erro-stackoverflow" />
+                    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://pt.stackoverflow.com/questions/59135/erro-stackoverflow" />
+                    <meta property="og:site_name" content="Stack Overflow em Portugu&#234;s" />
+                    <meta property="og:image" itemprop="image primaryImageOfPage" content="https://cdn.sstatic.net/Sites/br/img/apple-touch-icon@2.png?v=2bb2b60be04f" />
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:domain" content="pt.stackoverflow.com" />
+                    <meta name="twitter:site" content="@StackOverflowPT" />
+                    <meta name="twitter:creator" content="@StackOverflowPT" />
+                    <meta name="twitter:title" property="og:title" itemprop="name" content="Erro StackOverflow" />
                 </Head>
                 <body>
                     <Main />
